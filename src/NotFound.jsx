0f742fc9c2f6f9fd786111b8148b7e73cc5dbabe
PRATYUSHDAWN <pratoss32@gmail.com>
2024-11-07 20:04:33 +0530

    g

--- src/NotFound.jsx
+++ src/NotFound.jsx
@@ -11,7 +11,7 @@ const NotFound = () => {
     return (
         <div>
             <section className="bg-[#2F2F2F] dark:bg-gray-900 ">
-            <section className="bg-black dark:bg-gray-900">
+            
                 <div className="container min-h-screen px-6 py-12 mx-auto lg:flex lg:items-center lg:gap-12">
                     <div className="w-full lg:w-1/2">
                         <p className="text-sm font-medium text-blue-500 dark:text-blue-400">404 error</p>
@@ -32,7 +32,7 @@ const NotFound = () => {
                         <img className="w-full max-w-lg lg:mx-auto invert brightness" src={logo} alt="404 Not Found" />
                     </div> 
                 </div>
-            </section>
+            
             </section>
         </div>
     );
